9437f54ea298c5f93ba12b0b38054e36c9d605c5
hun2 <hun2up@gmail.com>
2023-09-13 20:33:46 +0900

    start

--- pages/8_aug.py
+++ pages/8_aug.py
@@ -13,15 +13,15 @@ from utils import load_data, fig_linechart
 df_august = load_data(st.secrets["aug_url"]).drop(columns=['SUNAB_PK','납입회차','납입월도','영수유형','확정자','확정일','월보험료','환산월초','인정실적','실적구분','이관일자','확정유형','계약상태','최초등록일'])
 df_august['영수/환급일'] = pd.to_datetime(df_august['영수/환급일'])
 df_august['영수/환급보험료'] = pd.to_numeric(df_august['영수/환급보험료'], errors='coerce')
-df_insurance = df_august.groupby(['보험종목','영수/환급일'])['영수/환급보험료'].sum().reset_index(name='매출액')
+df_insurance = df_august.groupby(['보험종목'])['영수/환급보험료'].sum().reset_index(name='매출액')
 st.dataframe(df_insurance)
 
 '''
 list_linechart[0]: dataframe (df_stat, df_trnd)
 list_linechart[1]: 참조 컬럼 (소속부문, 입사연차, 과정명)
 list_linechart[2]: 데이터 (신청인원, 신청누계, 수료인원, 수료누계, 수료율, IMO신청률 등)
-list_linechart[3]: 차트 제목
-list_linechart[4]: df_apply: '월' / df_attend: '날짜'
+list_linechart[3]: df_apply: '월' / df_attend: '날짜'
+list_linechart[4]: 차트 제목
 '''
 
 list_line_insuarance = [df_insurance, '보험종목', '매출액', '영수/환급일', '보험종목별 매출액 추이']
