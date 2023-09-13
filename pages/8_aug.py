850263cbacda92b4ca81a266923822caebe71728
hun2 <hun2up@gmail.com>
2023-09-13 20:44:13 +0900

    start

--- pages/8_aug.py
+++ pages/8_aug.py
@@ -12,9 +12,9 @@ from utils import load_data, fig_linechart
 # 출석부 데이터베이스 호출 (교육과정수료현황) & 컬럼 삭제 (번호)
 df_august = load_data(st.secrets["aug_url"]).drop(columns=['SUNAB_PK','납입회차','납입월도','영수유형','확정자','확정일','환산월초','인정실적','실적구분','이관일자','확정유형','계약상태','최초등록일'])
 # df_august['영수/환급일'] = pd.to_datetime(df_august['영수/환급일'])
-df_august['월보험료'] = pd.to_numeric(df_august['월보험료'], errors='coerce')
-# df_insurance = df_august.groupby(['보험종목']).count()
-df_insurance = df_august.groupby(['보험종목','영수/환급일'])['월보험료'].sum().reset_index(name='매출액')
+# df_august['월보험료'] = pd.to_numeric(df_august['월보험료'], errors='coerce')
+df_insurance = df_august.groupby(['보험종목']).count()
+# df_insurance = df_august.groupby(['보험종목','영수/환급일'])['월보험료'].sum().reset_index(name='매출액')
 
 '''
 list_linechart[0]: dataframe (df_stat, df_trnd)
@@ -24,8 +24,8 @@ list_linechart[3]: df_apply: '월' / df_attend: '날짜'
 list_linechart[4]: 차트 제목
 '''
 
-list_line_insuarance = [df_insurance, '보험종목', '매출액', '영수/환급일', '보험종목별 매출액 추이']
-fig_line_insurnace = fig_linechart(list_line_insuarance)
+# list_line_insuarance = [df_insurance, '보험종목', '매출액', '영수/환급일', '보험종목별 매출액 추이']
+# fig_line_insurnace = fig_linechart(list_line_insuarance)
 
 ########################################################################################################################
 ################################################     메인페이지 설정     ###################################################
@@ -37,7 +37,7 @@ st.header("8월 매출현황")
 # -----------------------------------------------------  차트 노출  ---------------------------------------------------------
 st.markdown("<hr>", unsafe_allow_html=True)
 st.dataframe(df_insurance)
-st.plotly_chart(fig_line_insurnace, use_container_width=True)
+# st.plotly_chart(fig_line_insurnace, use_container_width=True)
 
 ########################################################################################################################
 ###########################################     stremalit 워터마크 숨기기     ##############################################
