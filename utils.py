5eef00adfbe7a9d583b7b55216f375ba12c57f1b
hun2 <hun2up@gmail.com>
2023-09-13 20:02:11 +0900

    start

--- utils.py
+++ utils.py
@@ -14,3 +14,32 @@ from datetime import datetime
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
+
+'''
+list_linechart[0]: dataframe (df_stat, df_trnd)
+list_linechart[1]: 참조 컬럼 (소속부문, 입사연차, 과정명)
+list_linechart[2]: 데이터 (신청인원, 신청누계, 수료인원, 수료누계, 수료율, IMO신청률 등)
+list_linechart[3]: 차트 제목
+list_linechart[4]: df_apply: '월' / df_attend: '날짜'
+'''
+def fig_linechart(list_linechart):
+    fig_line = pl.graph_objs.Figure()
+    # Iterate over unique channels and add a trace for each
+    for reference in list_linechart[0][list_linechart[1]].unique():
+        line_data = list_linechart[0][list_linechart[0][list_linechart[1]] == reference]
+        fig_line.add_trace(pl.graph_objs.Scatter(
+            x=line_data[list_linechart[3]],
+            y=line_data[list_linechart[2]],
+            mode='lines+markers',
+            name=reference,
+        ))
+    # Update the layout
+    fig_line.update_layout(
+        title=list_linechart[4],
+        xaxis_title=list_linechart[3],
+        yaxis_title=list_linechart[2],
+        legend_title=list_linechart[1],
+        hovermode='x',
+        template='plotly_white'  # You can choose different templates if you prefer
+    )
+    return fig_line
