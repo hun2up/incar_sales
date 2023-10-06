73a0ec2f7a1108c1d2142c9c362df8f44aa191f3
hun2 <hun2up@gmail.com>
2023-10-06 15:29:55 +0900

    rank

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -238,7 +238,10 @@ if authentication_status:
     st.write("매출액 상위 FA별 판매상품 TOP5")
     fa1 = st.columns(5)
     for i in range(5):
-        fa1[i].metric(df_fa1.iat[i,3] + ' (' + df_fa1[i,4], df_fa1[i,5] + '원')
+        if fa1[i].metric(df_fa1.iat[i,3] + ' (' + df_fa1[i,4], df_fa1[i,5] + '원') == True:
+            fa1[i].metric(df_fa1.iat[i,3] + ' (' + df_fa1[i,4], df_fa1[i,5] + '원')
+        else:
+            break
     
     fa2 = st.columns(5)
     for i in range(5):
