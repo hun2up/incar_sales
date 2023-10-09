5554114f9ea8a6be4df247770498330ed6416438
hun2 <hun2up@gmail.com>
2023-10-09 18:05:05 +0900

    code modifying

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -140,10 +140,17 @@ if authentication_status:
     st.markdown("## 전체 현황 요약")
 
     # 소속부문 매출액 순위는 금액 단위가 커서 '원' 생략
-    st.markdown("#### 소속부문 매출액 순위")
+    chn = st.columns([2,1,1,1])
+    chn[0].markdown("#### 부문 매출액 순위")
     chn = st.columns(6)
     for i in range(6):
         chn[i].metric(dfr_chn.iat[i, 0], dfr_chn.iat[i, 1])
+    if chn[1].toggle("부문별 매출액 상위 FA"):
+        st.markdown("##### 부문별 매출액 상위 FA")
+    if chn[2].toggle("부문별 매출액 상위 보험회사"):
+        st.markdown("##### 부문별 매출액 상위 보험회사")
+    if chn[3].toggle("부문별 매출액 상위 보험상품"):
+        st.markdown("##### 부문별 매출액 상위 보험상품")
 
     tgl_chn_fa = st.toggle("각 부문별 매출액 상위 TOP5 FA")
     tgl_chn_com = st.toggle("각 부문별 매출액 상위 TOP5 보험회사")
