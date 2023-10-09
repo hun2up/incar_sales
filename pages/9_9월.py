46b7ddf0ca79c4185c812b189c5205864cd923d4
hun2 <hun2up@gmail.com>
2023-10-09 17:09:15 +0900

    code modifying

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -163,7 +163,8 @@ if authentication_status:
     cat_prod = st.columns(5)
     cat[0].write("매출액 상위 TOP5 (상품군)")
     fn_ranking(dfr_cat, 'single', rcat)
-    cat[4].toggle("상품군별 매출액 순위")
+    if cat[4].toggle("상품군별 매출액 순위"):
+        fn_ranking(dfr_cat_cover, 'multiple', cat_prod)
 
 
     st.write("매출액 상위 TOP5 (보험상품)")
