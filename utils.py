d97487d972113b9cac15c4c505361bd2c8bde403
hun2 <hun2up@gmail.com>
2023-10-09 15:31:51 +0900

    code modifying

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def fn_call(v_month):
 def fn_category(dfv_month, category):
     # 필요컬럼, 영수일자, 영수/환급보험료로 묶고, 영수/환급보험료 합계 구한 뒤 컬럼명을 '매출액'으로 변경
     dfv_category = dfv_month.groupby(category)['영수/환급보험료'].sum().reset_index(name='매출액')
-    dfv_category['매출액'] = dfv_category['매출액'].map('{:,.0f}'.format)
+    # dfv_category['매출액'] = dfv_category['매출액'].map('{:,.0f}'.format)
     return dfv_category
 
 # ------------------------------------------------    손생 합계    -------------------------------------------------------
