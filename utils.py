6aa68a54f4cd08c6d98d35bacb5fabdf143113ad
hun2 <hun2up@gmail.com>
2023-10-10 16:04:09 +0900

    displot

--- utils.py
+++ utils.py
@@ -155,7 +155,6 @@ def fn_peformance(df_month, this_month):
     st.plotly_chart(fig_linechart(df_insu, '보험종목별 매출액 추이'), use_container_width=True)
 
     # ---------------------------------------------  생손매출액 (히스토그램)  --------------------------------------------------
-    '''
     df_test = df_month.groupby(['보험종목','영수/환급보험료','증권번호'])['증권번호'].count().reset_index(name='구분')
     df_life = df_test[df_test['보험종목'].isin(['생명보험'])]
     df_life = df_life.rename(columns={'영수/환급보험료':'생명보험'})
@@ -163,7 +162,10 @@ def fn_peformance(df_month, this_month):
     df_fire = df_test[df_test['보험종목'].isin(['손해보험'])]
     df_fire = df_fire.rename(columns={'영수/환급보험료':'생명보험'})
     df_fire = df_fire.drop(columns=['보험종목','증권번호','구분'])
-    '''
+    data = [df_life, df_fire]
+    labels = ['생명보험','손해보험']
+    fig_displot = ff.create_distplot(data, labels, bin_size=.2)
+    st.plotly_chart(fig_displot, use_container_width=True)
 
     # -----------------------------------------  보험사별 매출액, 상품군별 매출액  ----------------------------------------------
     fig_line_company, fig_line_prod = st.columns(2)
