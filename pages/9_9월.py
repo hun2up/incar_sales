1acf095807bd1c7a7e1118d6dea18b7142113e65
hun2 <hun2up@gmail.com>
2023-10-13 16:45:20 +0900

    modify

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -214,7 +214,7 @@ if authentication_status:
     st.markdown('---') # 구분선
     prod = st.columns([2,1,1,1]) # 컬럼 나누기
     prod[0].markdown("#### 매출액 상위 보험상품") # 제목
-    instance_product_cards = MakeCard(dfr_prod, 5)
+    instance_product_cards = MakeCard(5)
     instance_product_cards.make_card_multiple()
     # make_cards(dfr_prod, 'multiple') # 메인랭킹 노출
     # 세부랭킹 (토글)
