302cd52adef62376f6b00b3a673e46e89af06376
hun2 <hun2up@gmail.com>
2023-10-16 15:53:04 +0900

    object

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -169,7 +169,7 @@ if authentication_status:
     st.markdown('---') # 구분선
     category = st.columns([2,1,1,1])
     category[0].markdown('#### 매출액 상위 상품군')
-    instance_category.make_card_multiple(df=instance_category.make_rankdata_class(columns=['상품군']), number=5)
+    instance_category.make_card_single(df=instance_category.make_rankdata_class(columns=['상품군']), number=5)
     # 세부랭킹 (토글)
     if category[1].toggle('상품군별 매출액 상위 지점'):
         instance_category.make_toggles_category(reference=['파트너','소속','상품군'], title='지점', form='multiple')
