e0b17739f8b70ed014ad56061ab0200b5d598dcb
hun2 <hun2up@gmail.com>
2023-10-17 12:11:12 +0900

    chart

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -76,9 +76,9 @@ if authentication_status:
     # instance_chart.select_columns_basic(column_select=['보험종목','영수일자'])
     # st.dataframe(instance_chart.select_columns_basic(column_select=['보험종목','영수일자']))
     instance_chart.select_columns_basic(column_select=['보험회사','영수일자'])
-    # st.dataframe(instance_chart.select_columns_basic(column_select=['보험회사','영수일자']))
+    st.dataframe(instance_chart.select_columns_basic(column_select=['보험회사','영수일자']))
     instance_chart.select_columns_basic(column_select=['상품군','영수일자'])
-    # st.dataframe(instance_chart.select_columns_basic(column_select=['상품군','영수일자']))
+    st.dataframe(instance_chart.select_columns_basic(column_select=['상품군','영수일자']))
 
     end_chart_after = time.time()
     st.write(f"시간측정(차트-수정후) : {end_chart_after - start_chart_after} sec")
