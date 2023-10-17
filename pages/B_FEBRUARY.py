1f193c1f5395bb55cb7e30876466368b7d504bc3
hun2 <hun2up@gmail.com>
2023-10-17 20:03:48 +0900

    chart

--- pages/B_FEBRUARY.py
+++ pages/B_FEBRUARY.py
@@ -58,7 +58,6 @@ if authentication_status == None:
 if authentication_status == False:
     st.error('아이디와 패스워드를 확인해주세요')
 if authentication_status:
-    # fn_peformance(df_month, this_month)
     ##########################################################################################################################
     ##############################################     메인페이지 타이틀     ##################################################
     ##########################################################################################################################
@@ -69,17 +68,22 @@ if authentication_status:
     ##################################################     차트 (현황)     ####################################################
     ##########################################################################################################################
     instance_chart = Charts(df=df_month)
-    st.plotly_chart(instance_chart.make_chart_line(df=instance_chart.make_data_sum(column_select=['보험종목','영수일자']), title='보험종목별 매출액 추이'), use_container_width=True)
+
+    sum_feb, sum_month = instance_chart.make_data_sum(column_select=['보험종목','영수일자'])
+    st.plotly_chart(instance_chart.make_chart_line(df=sum_month, title='보험종목별 매출액 추이'), use_container_width=True)
 
     # -----------------------------------------  보험사별 매출액, 상품군별 매출액  ----------------------------------------------
     fig_line_company, fig_line_product = st.columns(2)
-    fig_line_company.plotly_chart(instance_chart.make_chart_line(df=instance_chart.make_data_basic(column_select=['보험회사','영수일자']), title='보험회사별 매출액 추이'), use_container_width=True) # 보험회사별 매출액
-    fig_line_product.plotly_chart(instance_chart.make_chart_line(df=instance_chart.make_data_basic(column_select=['상품군','영수일자']), title='상품군별 매출액 추이'), use_container_width=True) # 상품군별 매출액
-
+    company_feb, company_month = instance_chart.make_data_basic(column_select=['보험회사','영수일자'])
+    product_feb, product_month = instance_chart.make_data_basic(column_select=['상품군','영수일자'])
+    fig_line_company.plotly_chart(instance_chart.make_chart_line(df=company_month, title='보험회사별 매출액 추이'), use_container_width=True) # 보험회사별 매출액
+    fig_line_product.plotly_chart(instance_chart.make_chart_line(df=product_month, title='상품군별 매출액 추이'), use_container_width=True) # 상품군별 매출액
+    
     # ---------------------------------------  소속부문별 매출액, 입사연차별 매출액  ---------------------------------------------
     fig_line_channel, fig_line_career = st.columns(2)
-    fig_line_channel.plotly_chart(instance_chart.make_chart_line(df=instance_chart.make_data_basic(column_select=['소속','영수일자']), title='소속부문별 매출액 추이') ,use_container_width=True)
-    
+    channel_feb, channel_month = instance_chart.make_data_basic(column_select=['소속','영수일자'])
+    fig_line_channel.plotly_chart(instance_chart.make_chart_line(df=channel_month, title='소속부문별 매출액 추이') ,use_container_width=True)
+
     ##########################################################################################################################
     ##############################################     스타일 카드 (랭킹)     #################################################
     ##########################################################################################################################
