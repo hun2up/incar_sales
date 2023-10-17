b8d0d1595dc83e789c3198dffc05e7951ed3afd5
hun2 <hun2up@gmail.com>
2023-10-17 18:43:14 +0900

    chart

--- main.py
+++ main.py
@@ -50,36 +50,27 @@ if authentication_status:
     ##########################################################################################################################
     ##################################################     차트 (현황)     ####################################################
     ##########################################################################################################################
-    month = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+    month = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
     start = time.time()
-    # df_year = pd.DataFrame()
-    # df_company = pd.DataFrame()
-    # df_product = pd.DataFrame()
-    # df_channel = pd.DataFrame()
-    for i in month:
-        st.write(i)
-        # df_month = call_data(i)
-    
-    # st.dataframe(df_month)
-
-    '''
-    for key in month_dict:
-        df_month = call_data(key)
-        instance_chart = Charts(df=df_month)
-        sum_year, sum_month = instance_chart.make_data_sum(column_select=['보험종목','영수일자'])
-        company_year, company_month = instance_chart.make_data_basic(column_select=['보험회사','영수일자'])
-        product_year, product_month = instance_chart.make_data_basic(column_select=['상품군','영수일자'])
-        channel_year, channel_month = instance_chart.make_data_basic(column_select=['소속','영수일자'])
-        df_year = pd.concat([df_year, sum_year], axis=0)
-        df_company = pd.concat([df_company, company_year], axis=0)
-        df_product = pd.concat([df_product, product_year], axis=0)
-        df_channel = pd.concat([df_channel, channel_year], axis=0)
+    df_year = pd.DataFrame()
+    df_company = pd.DataFrame()
+    df_product = pd.DataFrame()
+    df_channel = pd.DataFrame()
+    df_month = call_data("jan")
+    instance_chart = Charts(df=df_month)
+    sum_jan, sum_month = instance_chart.make_data_sum(column_select=['보험종목','영수일자'])
+    company_jan, company_month = instance_chart.make_data_basic(column_select=['보험회사','영수일자'])
+    product_jan, product_month = instance_chart.make_data_basic(column_select=['상품군','영수일자'])
+    channel_jan, channel_month = instance_chart.make_data_basic(column_select=['소속','영수일자'])
+    df_year = pd.concat([df_year, sum_jan], axis=0)
+    df_company = pd.concat([df_company, company_jan], axis=0)
+    df_product = pd.concat([df_product, product_jan], axis=0)
+    df_channel = pd.concat([df_channel, channel_jan], axis=0)
     
     st.dataframe(df_year)
     st.dataframe(df_company)
     st.dataframe(df_product)
     st.dataframe(df_channel)
-    '''
 
     end = time.time()
     st.write(f"시간측정 : {end - start}")
