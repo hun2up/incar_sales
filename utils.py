8c0de5fd6115f29c3c6eecd064e2800dce13f313
hun2 <hun2up@gmail.com>
2023-10-17 10:02:45 +0900

    chart

--- utils.py
+++ utils.py
@@ -85,19 +85,42 @@ class Charts:
 
     # ----------------------------    그래프 제작을 위한 필요 컬럼 분류하고 누적값 구하기    -----------------------------------
     def make_chart(self, column_select):
-        # 필요컬럼, 영수일자, 영수/환급보험료로 묶고, 영수/환급보험료 합계 구한 뒤 컬럼명을 '매출액'으로 변경
+        # 필요컬럼, 영수일자, 영수/환급보험료로 묶고, 영수/환급보험료 합계 구한 뒤 컬럼명을 '매출액'으로 변경 -> 생명보험 / 영수일자 / 매출액
         df_chart = self.df.groupby(column_select)['영수/환급보험료'].sum().reset_index(name='매출액')
         # 구분 컬럼 추가
         df_chart.columns.values[0] = '구분'
-        # 구분 고유값만 남기기 (보험종목, 보험회사 등)
+        # 구분 고유값만 남기기 (보험종목, 보험회사 등) -> 구분 / 개수
         df_present = df_chart.groupby(['구분'])['구분'].count().reset_index(name="개수")
-        # 영수일자 고유값만 남기기 (매출액 없어도 일자를 최대로 지정하기 위함)
+        # 영수일자 고유값만 남기기 (매출액 없어도 일자를 최대로 지정하기 위함) -> 영수일자 / 개수
         df_date = df_chart.groupby(['영수일자'])['영수일자'].count().reset_index(name="개수")
-        # 보험회사 또는 보험종목 개수 만큼 반복문 실행 위해 리스트 제작
+        # 보험회사 또는 보험종목 개수 만큼 반복문 실행 위해 리스트 제작 -> 구분
         running = df_present['구분'].tolist()
-        # 반복문 실행을 위한 초기 데이터프레임 제작
+        # 반복문 실행을 위한 초기 데이터프레임 제작 -> 구분 / 영수일자 / 매출액
         df_total = pd.DataFrame(columns=['구분','영수일자','매출액'])
-        st.dataframe(running)
+        # 반복문 실행을 위한 구간 선언 
+        for i in range(len(running)):
+            # 생명보험이나 손해보험만 남기기
+            df_base = df_chart[df_chart.iloc[:,0] == running[i]]
+            df_running = df_base.merge(df_date, on='영수일자', how='right')
+            st.dataframe(df_base)
+            st.dataframe(df_running)
+            '''
+            # 최대한의 날짜프레임에 보험사별 매출현황 끼워넣기
+            for insert in range(df_running.shape[0]):
+                if pd.isna(df_running.iloc[insert, 0]):
+                    df_running.iloc[insert,0] = running[i]
+                    df_running.iloc[insert,2] = 0
+                else:
+                    pass
+            # 누적매출액 구하기
+            for running in range(df_running.shape[0]):
+                try:
+                    df_running.iloc[running+1,2] = df_running.iloc[running+1,2] + df_running.iloc[running,2]
+                except:
+                    pass
+            df_total = pd.concat([df_total, df_running], axis=0)
+        return df_total
+        '''
                 
 
 # 이거 너무 복잡함 (절차지향적임)
