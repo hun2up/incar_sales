0daa7f528f55d69e8a823e7565cb1f64bd083b8b
hun2 <hun2up@gmail.com>
2023-10-23 12:43:26 +0900

    chart

--- pages/9_9월.py
+++ pages/9_9월.py
@@ -69,12 +69,8 @@ if authentication_status:
     ##########################################################################################################################
     instance_chart = Charts(df=df_month)
 
-
     sum_year, sum_month = instance_chart.make_data_sum(column_select=['보험종목','영수일자'])
     st.plotly_chart(instance_chart.make_chart_line(df=sum_month, title='보험종목별 매출액 추이'), use_container_width=True)
-
-    
-    st.plotly_chart(instance_chart.make_chart_merged(df=sum_month, title='제목'), use_container_width=True)
     st.plotly_chart(instance_chart.make_chart_stacked(), use_container_width=True)
 
     # -----------------------------------------  보험사별 매출액, 상품군별 매출액  ----------------------------------------------
